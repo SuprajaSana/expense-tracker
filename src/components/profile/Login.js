d99a259201fd7b9c5ed68c232468d828b8caf2c6
SuprajaSana <sanasupraja2727@gmail.com>
2022-11-19 12:11:08 +0530

    second commit

--- src/components/profile/Login.js
+++ src/components/profile/Login.js
@@ -1,118 +1,130 @@
-import { useRef ,useState,useContext } from "react";
+import { useRef, useState, useContext } from "react";
 import AuthContext from "../../store/auth-context";
 import { useHistory } from "react-router-dom";
 
 import classes from "./Login.module.css";
 
 const Login = () => {
-    const emailInputRef = useRef("");
-    const passwordInputRef = useRef("");
-    const ConfirmpasswordInputRef = useRef("");
+  const emailInputRef = useRef("");
+  const passwordInputRef = useRef("");
+  const ConfirmpasswordInputRef = useRef("");
 
-    const [isLogin, setIsLogin] = useState(true);
-    const [isSendingRequest, setSendingRequest] = useState(false);
-    const [Error,setError]=useState(false)
+  const [isLogin, setIsLogin] = useState(true);
+  const [isSendingRequest, setSendingRequest] = useState(false);
+  const [Error, setError] = useState(false);
 
-    const authCtx = useContext(AuthContext)
-    
-    const history = useHistory();
+  const authCtx = useContext(AuthContext);
 
-    const switchAuthModeHandler = () => {
-        setIsLogin((prevState) => !prevState);
-    };
+  const history = useHistory();
 
-    const submitFormHandler = (e) => {
-        e.preventDefault();
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
 
-        const emailValue = emailInputRef.current.value;
-        const passwordValue = passwordInputRef.current.value;
-        const ConfirmpasswordValue = ConfirmpasswordInputRef.current.value;
+  const submitFormHandler = (e) => {
+    e.preventDefault();
 
-        setSendingRequest(true);
+    const emailValue = emailInputRef.current.value;
+    const passwordValue = passwordInputRef.current.value;
+    const ConfirmpasswordValue = ConfirmpasswordInputRef.current.value;
 
-        if (passwordValue != ConfirmpasswordValue) {
-            setError(true);
-            alert("Password does not match");
-            setSendingRequest(false);
+    setSendingRequest(true);
+
+    if (passwordValue != ConfirmpasswordValue) {
+      setError(true);
+      alert("Password does not match");
+      setSendingRequest(false);
+    } else {
+      setSendingRequest(true);
+      if (isLogin) {
+        fetch(
+          "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAXlNUd-_iFiS1IigjkhHLNYUq2wkiqhec",
+          {
+            method: "POST",
+            body: JSON.stringify({
+              email: emailValue,
+              password: passwordValue,
+              returnSecureToken: true,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        ).then((response) => {
+          setSendingRequest(false);
+          if (response.ok) {
+              return response.json()
           } else {
-            setSendingRequest(true);
-            if (isLogin) {
-                fetch("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAXlNUd-_iFiS1IigjkhHLNYUq2wkiqhec", {
-                    method: "POST",
-                    body: JSON.stringify({
-                        email: emailValue,
-                        password: passwordValue,
-                        returnSecureToken: true,
-                    }),
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                })
-                    .then((response) => {
-                        setSendingRequest(false);
-                        if (response.ok) {
-                            console.log(response)
-                            authCtx.login(response.idToken);
-                            history.replace("/welcome");
-                        } else {
-                            response.json().then((data) => {
-                                let errorMessage = "Authentication Failed!";
-                                if (data && data.error && data.error.message) {
-                                    errorMessage = data.error.message;
-                                }
-                                alert(errorMessage);
-                            });
-                        }
-                    })
-            }
-        }
+            response.json().then((data) => {
+              let errorMessage = "Authentication Failed!";
+              if (data && data.error && data.error.message) {
+                errorMessage = data.error.message;
+              }
+              alert(errorMessage);
+            });
+          }
+        })
+            .then((data) => {
+            console.log(data.idToken)
+            authCtx.login(data.idToken);
+            //authCtx.login(data.idToken);
+            history.replace("/welcome");
+          })
+          .catch((err) => {
+            throw new Error(err.message);
+          });
+      }
     }
+  };
 
-    return (
-        <section className={classes.auth}>
-            <h1>Login</h1>
-            <form onSubmit={submitFormHandler}>
-                <div className={classes.control}>
-                    <input type="email" id="email" placeholder="Email" required ref={emailInputRef} />
-                </div>
-                <div className={classes.control}>
-                    <input
-                        type="password"
-                        id="password"
-                        placeholder="Password"
-                        required
-                        ref={passwordInputRef}
-                    />
-                </div>
-                <div className={classes.control}>
-                    <input
-                        type="Confirm password"
-                        id="Confirm password"
-                        placeholder="Confirm Password"
-                        required
-                        ref={ConfirmpasswordInputRef}
-                    />
-                </div>
-                <div className={classes.actions}>
-                    {!isSendingRequest && (
-                        <button>Login</button>
-                    )}
-                    {isSendingRequest && <p>Sending Request</p>}
-                    <button
-                        type="button"
-                        className={classes.toggle}
-                        onClick={switchAuthModeHandler}
-                    >
-                    </button>
-                    <span>Forgot Password</span>
-                </div>
-                <div>
-                    <span>Don't have an account?</span>
-                    <span>SignUp</span>
-                </div>
-            </form>
-        </section>
-    )
-}
+  return (
+    <section className={classes.auth}>
+      <h1>Login</h1>
+      <form onSubmit={submitFormHandler}>
+        <div className={classes.control}>
+          <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            required
+            ref={emailInputRef}
+          />
+        </div>
+        <div className={classes.control}>
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            required
+            ref={passwordInputRef}
+          />
+        </div>
+        <div className={classes.control}>
+          <input
+            type="Confirm password"
+            id="Confirm password"
+            placeholder="Confirm Password"
+            required
+            ref={ConfirmpasswordInputRef}
+          />
+        </div>
+        <div className={classes.actions}>
+          {!isSendingRequest && <button>Login</button>}
+          {isSendingRequest && <p>Sending Request</p>}
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          ></button>
+          <span>Forgot Password</span>
+        </div>
+        <div>
+          <span>Don't have an account?</span>
+          <span>SignUp</span>
+        </div>
+      </form>
+    </section>
+  );
+};
 
 export default Login;
